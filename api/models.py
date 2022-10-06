d7fefa0eeaaebb2aa1917bf12429642a702f2fe5
Daniel <danielmc__@hotmail.com>
2022-10-06 15:43:42 -0300

    tabela de meta e tipo_midia

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TipoMidia(models.Model):
+    tipo = models.CharField(max_length=30)
+
+class Meta(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tipomidia = models.ForeignKey(TipoMidia, on_delete=models.CASCADE)
+    criador = models.ForeignKey(User, on_delete=models.CASCADE, related_name='criador')
+    quantidade_objetivo = models.BigIntegerField()
+    quantidade_atual = models.BigIntegerField()
+    data_inicio = models.DateField(auto_now_add=True)
+    data_limite = models.DateField()
+    data_conclusao = models.DateField(blank=True, null=True)
+    is_ativa = models.BooleanField()
+    is_cumprida = models.BooleanField()
+
--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,14 @@ from . import views
 urlpatterns = [
     path('', views.routes, name='routes'),
     path('unauthorized', views.unauthorized, name='unauthorized'),
-    path('user', views.user, name='users'),
-    path('user/<str:pk>', views.user_by_id, name='user'),
+
+    path('user', views.user, name='user'),
+    path('user/<str:pk>', views.user_by_id, name='user_by_id'),
     path('user/find/<str:pk>', views.user_by_username_or_email, name='user_by_username_or_email'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
+
+    # path('meta', views.meta, name='meta'),
+    # path('meta/<str:pk>', views.meta_by_id, name='meta_by_id'),
+    # path('meta/find/<str:pk>', views.meta_by_user, name='meta_by_user'),
 ]
